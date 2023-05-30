0ddcdb6ebf9bbf08c13da3cd41e3643410b62340
Katlego Mothibe <Kaymothibe3@gmail.com>
2023-05-30 15:43:12 +0200

    unit tests

--- electricity.dom.js
+++ electricity.dom.js
@@ -9,8 +9,11 @@ const topUpBtn = document.querySelector('.topupNow');
 const useBtn = document.querySelector('.useNow');
 
 // Factory Function instance 
-// const electricity =  Electricity();
+const electricity = Electricity();
 
+//loop through all the radio buttons for the top up amount
+//use a event listener for the changes and get the checked button
+//assign the value of the checked button to the the checkedAmount variable
 let checkedAmount = 0;
 topUpAmountRadBtns.forEach(btn =>{
     btn.addEventListener('change', ()=>{
@@ -26,6 +29,10 @@ topUpAmountRadBtns.forEach(btn =>{
     })
 })
 
+
+//loop through all the appliances radio buttions
+//use an event listener to listen for changes and check the checked button
+//assign the value of the checked button to the checkedAppliance variable
 let checkedAppliance = '';
 applianceRadBtns.forEach(btn =>{
     btn.addEventListener('change', ()=>{
@@ -36,16 +43,23 @@ applianceRadBtns.forEach(btn =>{
     })
 })
 
+//retrieve all the values from local stoarge or just get 0
 let unitsAvailable = parseInt(localStorage.getItem('unitsAvailable')) || 0;
 let boughtUnits = parseInt(localStorage.getItem('boughtUnits')) || 0;
 let totalAmountSpent = parseInt(localStorage.getItem('totalAmountSpent')) || 0;
 let advanceTaken = localStorage.getItem('advanceTaken') === 'true';
 let oustandingAmout = parseInt(localStorage.getItem('oustandingAmout')) || 0;
 
+//update the values upon refresh from localStorage
 unitsAvailableEl.innerHTML = unitsAvailable;
 totalUnitsEl.innerHTML = boughtUnits;
 totalAmountEl.innerHTML = totalAmountSpent;
 
+
+//use this function to update the values in the middle screen
+//have the units and amount parameters
+//if there is an adavance hadndle the amout changes or oustanding advance payemnts
+//if there is no advance update the middle screen values with the parameters and set all the values to local storage
 function updateMiddleScreen(units, amount){
     if(advanceTaken === true){
         oustandingAmout -= amount;
@@ -54,6 +68,7 @@ function updateMiddleScreen(units, amount){
             advanceTaken = false;
         }
         if(amount === 50){
+            oustandingAmout += 20;
             unitsAvailable += 14;
             boughtUnits += 14;
             totalAmountSpent += 20;
@@ -62,6 +77,7 @@ function updateMiddleScreen(units, amount){
             totalAmountEl.innerHTML = totalAmountSpent;
         }
         else if(amount === 20 && oustandingAmout <= 0){
+            oustandingAmout += 10;
             unitsAvailable += 7;
             boughtUnits += 7;
             totalAmountSpent += 10;
@@ -87,6 +103,9 @@ function updateMiddleScreen(units, amount){
     localStorage.setItem('oustandingAmout', oustandingAmout.toString());
 }
 
+
+//use this function to top up values according to the checked amount
+//for different amounts use the above function to update amounts and screen values
 function topUp(){
     switch(checkedAmount){
         case 10:
@@ -124,7 +143,7 @@ function topUp(){
     localStorage.setItem('oustandingAmout', oustandingAmout.toString());
 }
 
-
+//have a mappinng of how much eletricity units each appliance consumes
 let appliancesUsage = {
     Stove: 10, 
     Kettle: 5, 
@@ -132,6 +151,8 @@ let appliancesUsage = {
     Fridge: 13
 };
 
+//use this function to use eletricity units according to the selected appliance
+//upadte the screen values and local stoarge values
 function useEletricity(){
     if(checkedAppliance === 'stove' && unitsAvailable > appliancesUsage.Stove){
         unitsAvailable -= 10;
--- electricity.js
+++ electricity.js
@@ -1,5 +1,9 @@
 function Electricity() {
-
+    let unitsAvailable = 0;
+    let amountSpent = 0;
+    let unitsBought = 0;
+    let hasTakenAdvance = false;
+    let oustandingAmout = 0;
     // do we want to go with this or array? 
     let appliances = {
         'Stove': 10, 
@@ -8,9 +12,54 @@ function Electricity() {
         'Fridge': 13
     };
 
-    function topUpElectricity(amount) {
-        
+    function updateAmounts(units, amount){
+        if(hasTakenAdvance === true){
+            oustandingAmout -= amount;
+            
+            if(oustandingAmout <= 0){
+                hasTakenAdvance = false;
+            }
+            if(amount === 50){
+                oustandingAmout += 20;
+                unitsAvailable += 14;
+                unitsBought += 14;
+                amountSpent += 20;
+            }
+            else if(amount === 20 && oustandingAmout <= 0){
+                oustandingAmout += 10;
+                unitsAvailable += 7;
+                unitsBought += 7;
+                amountSpent += 10;
+            }
+        }
+        else{
+            unitsAvailable += units;
+            unitsBought += units;
+            amountSpent += amount;
+        }
 
+    }
+
+    function topUpElectricity(amount) {
+        if(amount === 0){
+            unitsAvailable = 0;
+        }
+        else if(amount === 10){
+            updateAmounts(7, 10);
+        }
+        else if(amount === 20){
+            updateAmounts(14, 20);
+        }
+        else if(amount === 50){
+            updateAmounts(35, 50)
+        }
+        else if(amount === 'advance' || amount === 30){
+            if(hasTakenAdvance === false){
+                oustandingAmout += 30;
+                updateAmounts(21, 30);
+                hasTakenAdvance = true;
+            }
+        }
     }
 
     function getUnitsAvailable() {
@@ -22,16 +71,37 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+        if(appliance === 'Stove' && unitsAvailable > appliances.Stove){
+            unitsAvailable -= 10;
+            return true;
+        }
+        else if(appliance === 'TV' && unitsAvailable > appliances.TV){
+            unitsAvailable -= 3;
+            return true;
+        }
+        else if(appliance === 'Fridge' && unitsAvailable > appliances.Fridge){
+            unitsAvailable -= 13;
+            return true;
+        }
+        else if(appliance === 'Kettle' && unitsAvailable > appliances.Kettle){
+            unitsAvailable -= 5;
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     function advanceTaken() {
+        return hasTakenAdvance;
     }
 
     function totalAmountSpent() {
+        return amountSpent;
     }
 
     function totalUnitsBought(){
+        return unitsBought;
     }
 
     return {
